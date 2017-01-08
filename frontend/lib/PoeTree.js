fc2a505f8de72be0e936437bf3b9636dc7bf90f9
eps1lon <silbermann.sebastian@gmail.com>
2017-01-08 19:39:36 +0100

    added support for blacklisting nodes when drawing

--- frontend/lib/PoeTree.js
+++ frontend/lib/PoeTree.js
@@ -1,4 +1,55 @@
+const blacklistNone = function () {
+    return false
+};
+
 class PoeTree {
+    /**
+     * checks if the edge between these nodes is Path of X to X edge
+     *
+     * @param source
+     * @param target
+     * @returns {boolean}
+     */
+    static scionPathOfEdge(source, target) {
+        return source.ascendancy != target.ascendancy
+    }
+
+    /**
+     * computes a svg path for an arc between 2 nodes
+     * @param start {PoeNode}
+     * @param end {PoeNode}
+     * @returns {string}
+     */
+    static describeArc(start, end){
+        const x = start.group.x;
+        const y = start.group.y;
+        const r = start.radius;
+        const tau = 2 * Math.PI;
+
+        let start_angle = start.angle;
+        let end_angle = end.angle;
+
+        if (start_angle > end_angle){
+            [start_angle, end_angle] = [end_angle, start_angle]
+        }
+
+        if (end_angle - start_angle > tau) {
+            end_angle = tau - Number.EPSILON;
+        }
+
+        let sweep = 0;
+        if (end_angle - start_angle > Math.PI) {
+            sweep = 1;
+        }
+
+        return [
+            //'M', x, y,
+            'M', x + Math.cos(start_angle) * r, y - (Math.sin(start_angle) * r),
+            'A', r, r, 0, 0, sweep, x + Math.cos(end_angle) * r, y - (Math.sin(end_angle) * r),
+            //'L', x, y
+        ].join(' ');
+    }
+
     constructor(tree_data) {
         this.data = tree_data;
         this.groups = new Map(Object.entries(this.data.groups));
@@ -31,17 +82,6 @@ class PoeTree {
             ]
 
         }
-    }
-
-    /**
-     * checks if the edge between these nodes is Path of X to X edge
-     *
-     * @param source
-     * @param target
-     * @returns {boolean}
-     */
-    static scionPathOfEdge(source, target) {
-        return source.ascendancy != target.ascendancy
     }
 
     /**
@@ -110,16 +150,18 @@ class PoeTree {
      *
      * @param d3_svg
      */
-    drawNodes(d3_svg) {
+    drawNodes(d3_svg, blacklist_fn = blacklistNone) {
         for (let [node_id, node] of this.nodes) {
-            d3_svg.append("circle")
-                .attr("r", 60)
-                .attr("cx", node.x)
-                .attr("cy", node.y)
-                .attr("class", ["tree_node", ...node.types].join(" "))
-                .attr("id", `node_${node_id}`)
-                .append("svg:title")
-                .text(node.inspect)
+            if (!blacklist_fn(node)) {
+                d3_svg.append("circle")
+                    .attr("r", 60)
+                    .attr("cx", node.x)
+                    .attr("cy", node.y)
+                    .attr("class", ["tree_node", ...node.types].join(" "))
+                    .attr("id", `node_${node_id}`)
+                    .append("svg:title")
+                    .text(node.inspect)
+            }
         }
     }
 
@@ -128,20 +170,13 @@ class PoeTree {
      *
      * @param d3_svg
      * @param blacklist_fn a callback that given the source and the target should return true
-     *                       if you dont want the edge to be drawn
+     *                       if you don't want the edge to be drawn
      */
-    drawEdges(d3_svg, blacklist_fn) {
-        if (!blacklist_fn) {
-            blacklist_fn = function () {
-                return false
-            }
-        }
-
-
-
+    drawEdges(d3_svg, blacklist_fn = blacklistNone) {
         for (let node of this.nodes.values()) {
             const x1 = node.x;
             const y1 = node.y;
+
             for (let adj_id of node.adjacent) {
                 const adj = this.nodes.get(adj_id);
 
@@ -196,35 +231,5 @@ class PoeTree {
      */
     yScaled(y, new_height) {
         return (y - this.dimensions[1]) * new_height / this.height
-    }
-
-    static describeArc(start, end){
-        const x = start.group.x;
-        const y = start.group.y;
-        const r = start.radius;
-        const tau = 2 * Math.PI;
-
-        let start_angle = start.angle;
-        let end_angle = end.angle;
-
-        if (start_angle > end_angle){
-            [start_angle, end_angle] = [end_angle, start_angle]
-        }
-
-        if (end_angle - start_angle > tau) {
-            end_angle = tau - Number.EPSILON;
-        }
-
-        let sweep = 0;
-        if (end_angle - start_angle > Math.PI) {
-            sweep = 1;
-        }
-
-        return [
-            //'M', x, y,
-            'M', x + Math.cos(start_angle) * r, y - (Math.sin(start_angle) * r),
-            'A', r, r, 0, 0, sweep, x + Math.cos(end_angle) * r, y - (Math.sin(end_angle) * r),
-            //'L', x, y
-        ].join(' ');
     }
 }
