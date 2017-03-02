1798cfbef7ae3aacd0074c64da6d680b6dfff109
eps1lon <silbermann.sebastian@gmail.com>
2017-03-02 10:55:23 +0100

    immutable improvements used to create a new instance every time the reducer was called

--- src/browser/reducers/app.js
+++ src/browser/reducers/app.js
@@ -1,10 +1,14 @@
 import { Map } from 'immutable';
 
-const app = () => {
-  return Map({
+const initial = Map({
+  dimensions: Map({
     width: 1900,
     height: 1550,
-  });
+  }),
+});
+
+const app = () => {
+  return initial;
 };
 
 export default app;
